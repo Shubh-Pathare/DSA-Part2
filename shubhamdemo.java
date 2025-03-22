32c152c45f1c0b20caae0444e19e53997e18bf86
Shubh-Pathare <pathareshubham2003gmail.com>
2025-03-22 16:15:28 +0530

    elcome

--- shubhamdemo.java
+++ shubhamdemo.java
@@ -1,6 +1,6 @@
 public class shubhamdemo {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hgggello");
         System.out.println("world");
     }
 }
